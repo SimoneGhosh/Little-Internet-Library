3959aa583199f06e48fd2af416f42c88e71c984b
Simone Ghosh <simone.ghoshbd@gmail.com>
2026-04-28 22:39:23 -0400

    form

--- frontend/src/components/Swap.jsx
+++ frontend/src/components/Swap.jsx
@@ -1,11 +1,42 @@
-import { Outlet } from 'react-router-dom';
+import { ErrorBoundary } from "react-error-boundary";
+import { useActionState } from "react";
+import './Swap.css';
 
-const Swap = () => {
-    return (
-        <main className="Swap">
-            <h1>Swap</h1>
-        </main>
-    )
+export default function Swap() {
+  async function bookSwap(prevState, formData) {
+    "use server";
+    const title = formData.get("title");
+    const author = formData.get("author");
+    try {
+      //await signUpNewUser(email);
+      console.log(`Swapping "${title}" by ${author}`);
+    } catch (err) {
+      return err.toString();
+    }
+  }
+
+  const [bookDetails, swapAction] = useActionState(bookSwap, null);
+  return (
+    <div className="swap-page">
+      <h1>Book Swap</h1>
+      <ErrorBoundary
+        fallback={<p>There was an error while searching for your book</p>}
+      >
+        <form action={swapAction} id="swap-form">
+          <label htmlFor="title">Title: </label>
+          <input name="title" id="title" placeholder="Book Title" />
+
+          <label htmlFor="author">Author: </label>
+          <input name="author" id="author" placeholder="First Name, Last Name" />
+
+          <label htmlFor="notes">Notes: </label>
+          <input name="notes" id="notes" placeholder="Leave a message for the next person :)" />
+
+          <button type="submit">Trade Now!</button>
+          {!!bookDetails && <p>{bookDetails}</p>}
+        </form>
+      </ErrorBoundary>
+    </div>
+  );
 }
 
-export default Swap;
